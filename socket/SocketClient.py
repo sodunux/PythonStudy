5860d38bdc012bbcdce1538d68ab64585453910a
sodunux <sodunux@163.com>
2018-05-24 08:55:15 +0800

    20180524
    初步实现在虚拟机上通信

--- socket/SocketClient.py
+++ socket/SocketClient.py
@@ -2,19 +2,18 @@
 import socket
 class SocketClient:
     def __init__(self):
-        self.addr=('192.168.1.3',50000)
+        self.port=('192.168.94.158',50000)
         self.sc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.sc.connect(self.addr)
+        self.sc.connect(self.port)
         print "connect success"
+
     def chat_server(self):
         print "chat_server"
         for i in range(100):
             self.sc.sendall("helloworld")
             data = self.sc.recv(1024)
-            print i 
             print "Received "+data
         self.sc.close()
-
 
 if __name__ == '__main__':
     sc=SocketClient()
--- socket/SocketServer.py
+++ socket/SocketServer.py
@@ -3,20 +3,20 @@ import socket
 class SocketServer:
     def __init__(self):
         self.ss=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.ss_port=('192.168.1.7',8080)
-        self.ss.bind(self.ss_port)
+        self.port=('192.168.94.158',50000)
+        self.ss.bind(self.port)
         self.ss.listen(5)
 
     def chat_client(self):
         conn,addr = self.ss.accept()
-        print "Connected by "+addr()
+        print "Connected by "+repr(addr)
         while 1:
             data = conn.recv(1024)
-            if data<=0:
+            if not data:
                 break
             else:
                 conn.sendall(data)
-            conn.close()
+        conn.close()
 
 if __name__ == '__main__':
     ss=SocketServer()
